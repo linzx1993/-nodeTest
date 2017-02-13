da33a35ac21ec87a889fc1a4a586549cb969284e
linzx <442765960@qq.com>
2017-02-13 14:12:34 +0800

    lzx

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 /**
  * Created by yiend on 2017/2/11.
  */
-var fs = require("fs");
+var fs = require("fs"),
+    stdin = process.stdin,
+    stdout = process.stdout;
 // console.log(fs.readdirSync(__dirname));//同步方式
 //异步方式
 fs.readdir(process.cwd(),function(err,files){
     console.log(files);
-    if(!files.length) {return console.log("没有文件")};
-    console.log("select files which file or directiory you want to see/n")
+    if(!files.length) {return console.log("没有文件")}
+    console.log("select files which file or directory you want to see/n");
     function file(i){
         var filename = files[i];
         fs.stat(__dirname + "/" + filename,function (err, stat) {
             if(stat.isDirectory()){
-                console.log('  ' + i + '\033[36m' + filename +'\033[39m]');
+                console.log('  ' + i + '\033[36m' + filename +'\033[39m');//\033[39m和\033[36m显示颜色
             }else{
-                console.log('  ' + i + '\090[36m' + filename +'\033[39m]');
+                console.log('  ' + i + '\033[90m' + filename +'\033[39m');
             }
-            i++;
-            if(i == files.length){
-                console.log('123');
-                process.stdout.write('  \033[33mEnter your choice: \033[39m');
-                process.stdin.resume();
+            if(++i == files.length){
+                read();
             }else{
                 file(i);
             }
         })
     }
+    function read(){
+        console.log(" ");
+        process.stdout.write('  \033[33mEnter your choice: \033[39m');
+        process.stdin.resume();
+        process.stdin.setEncoding("utf8")
+    }
     file(0);
-})
+});
