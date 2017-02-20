03052a0ca6e3c1d69289b6c67c04c5757f1daea8
linzx <442765960@qq.com>
2017-02-20 19:37:02 +0800

    test 2 breanch

--- http/readStream.js
+++ http/readStream.js
@@ -1,15 +1,19 @@
 /**
  * Created by yiend on 2017/2/20.
  */
-var http = require("http");
-var fs = require("fs");
-http.createServer(function(req,res){
-   res.writeHead(200,{"Content-Head" : "image/png"});
-   var stream = fs.createReadStream("image.png");
-   stream.on("data",function(data){
-       res.write(data);
-   });
-    stream.on("end",function(){
-        res.end();
-    })
+// var http = require("http");
+// var fs = require("fs");
+// http.createServer(function(req,res){
+//    res.writeHead(200,{"Content-Head" : "image/png"});
+//    var stream = fs.createReadStream("image.png");
+//    stream.on("data",function(data){
+//        res.write(data);
+//    });
+//     stream.on("end",function(){
+//         res.end();
+//     })
+// }).listen(3000);
+require("http").createServer(function(req,res){
+    res.writeHead(200,{"Contnet-Head" : "image/png"});
+    require("fs").createReadStream("image.png").pipe(res);
 }).listen(3000);
